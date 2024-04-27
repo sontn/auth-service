9562bde5ee1d6bb03e010be30a0f54092d9bc7eb
Son Tran Ngoc <son.tranngoc@gmail.com>
2024-04-27 17:08:43 +0700

    Generate jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constant';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: {
+        algorithm: 'RS256',
+        expiresIn: '60s',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
