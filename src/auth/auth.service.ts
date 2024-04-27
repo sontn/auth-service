6a4e144d6d5f72cf936562d2f1a15208b73a59fc
Son Tran Ngoc <son.tranngoc@gmail.com>
2024-04-27 16:05:46 +0700

    Create login function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    console.log(user);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const { password, ...result } = user;
+    return result;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,29 +6,29 @@ export type User = any;
 
 @Injectable()
 export class UsersService {
-    private readonly users: User[];
+  private readonly users: User[];
 
-    constructor() {
-        this.users = [
-            {
-                userId: 1,
-                username: 'john',
-                password: 'changeme',
-            },
-            {
-                userId: 2,
-                username: 'chris',
-                password: 'secret',
-            },
-            {
-                userId: 3,
-                username: 'maria',
-                password: 'guess',
-            },
-        ];
-    }
+  constructor() {
+    this.users = [
+      {
+        userId: 1,
+        username: 'sontran',
+        password: 'changeme',
+      },
+      {
+        userId: 2,
+        username: 'neo',
+        password: 'secret',
+      },
+      {
+        userId: 3,
+        username: 'peter',
+        password: 'guess',
+      },
+    ];
+  }
 
-    async findOne(username: string): Promise<User | undefined> {
-        return this.users.find(user => user.username === username);
-    }
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
 }
