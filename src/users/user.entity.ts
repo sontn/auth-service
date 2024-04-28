295c39ba1c7b06df2703886eaa741ed3cf07fa2c
Son Tran Ngoc <son.tranngoc@gmail.com>
2024-04-28 14:32:28 +0700

    Use postgres module and create user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,17 @@
+// create User Entity
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  tokenVersion: number;
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './user.entity';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT as any,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [User],
+    }),
+  ],
   providers: [UsersService],
   exports: [UsersService],
-
 })
 export class UsersModule {}
