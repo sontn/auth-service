e19912d9cedec537fbdb744d67c99cc6be7a27fa
Son Tran <son.tranngoc@gmail.com>
2024-05-08 16:19:47 +0700

    Ensure that the modules are modular

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,24 @@ import { AuthController } from './auth.controller';
 
 import { HashModule } from 'src/utilities/hash/hash.module';
 import { RepositoryModule } from 'src/repository/repository.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/utilities/jwt/constants';
 
 @Module({
-  imports: [HashModule, RepositoryModule],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: {
+        algorithm: 'RS256',
+        expiresIn: '300s',
+      },
+    }),
+    HashModule,
+    RepositoryModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -2,8 +2,22 @@ import { Module } from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 import { RepositoryModule } from 'src/repository/repository.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/utilities/jwt/constants';
 @Module({
-  imports: [RepositoryModule],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: {
+        algorithm: 'RS256',
+        expiresIn: '300s',
+      },
+    }),
+    RepositoryModule,
+  ],
   controllers: [BlogController],
   providers: [BlogService],
 })
