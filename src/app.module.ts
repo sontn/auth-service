fdac9e576f836ff07ba0cc709e1b9f91a7045200
Son Tran <son.tranngoc@gmail.com>
2024-05-08 16:23:14 +0700

    Remove duplicated codes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,8 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './utilities/jwt/constants';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: {
-        algorithm: 'RS256',
-        expiresIn: '300s',
-      },
-    }),
-    AuthModule,
-    BlogModule,
-  ],
+  imports: [AuthModule, BlogModule],
 })
 export class AppModule {}
