1090dcfcc8e7a297047b4e4932a58b9755077125
Son Tran <son.tranngoc@gmail.com>
2024-05-09 16:37:07 +0700

    Initialize Comment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ArticleModule } from './blog/article/article.module';
+import { CommentModule } from './blog/comment/comment.module';
 
 @Module({
-  imports: [AuthModule, ArticleModule],
+  imports: [AuthModule, ArticleModule, CommentModule],
 })
 export class AppModule {}
