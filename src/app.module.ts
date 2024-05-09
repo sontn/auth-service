ecfc4af174611f55e45258896de909a39661c5af
Son Tran <son.tranngoc@gmail.com>
2024-05-09 16:34:59 +0700

    Refactor directory to make clear

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { ArticleModule } from './article/article.module';
+import { ArticleModule } from './blog/article/article.module';
 
 @Module({
   imports: [AuthModule, ArticleModule],
