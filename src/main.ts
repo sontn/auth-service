0b8e1c1e6b7a22dfbd9c13e65001859e04858b36
Son Tran <son.tranngoc@gmail.com>
2024-05-13 12:01:43 +0700

    Update Pipes with whitelist property

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  setupGlobalPipes(app);
   // app.useGlobalFilters(new ExceptionLoggingFilter());
   await app.listen(3000);
 }
 bootstrap();
+
+function setupGlobalPipes(app: INestApplication) {
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      transform: true,
+      whitelist: true,
+    }
+  ));
+}
